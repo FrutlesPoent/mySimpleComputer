77c8cc946f78c08c62be5de9bb7f61ed4b4e0bdc
FrutlesPoent <frutlespoent@gmail.com>
2021-02-14 17:41:01 +0700

    Add some func

--- SimpleCompLibrary/main.c
+++ SimpleCompLibrary/main.c
@@ -5,31 +5,38 @@
 
 int main() {
 
-    int test;
+    // int test;
 
-    sc_memoryInit();
-    sc_regInit();
+    // sc_memoryInit();
+    // sc_regInit();
 
-    int commandMy;
-    int result = sc_commandEncode(10,50, &commandMy);
-    printf("True result encode = %d\n", result); // 0= true, 1 - false
+    // int commandMy;
+    // int result = sc_commandEncode(10,50, &commandMy);
+    // printf("True result encode = %d\n", result); // 0= true, 1 - false
 
-    result = sc_commandEncode(12,128, &commandMy);
-    printf("False result encode = %d\n", result);
+    // result = sc_commandEncode(12,128, &commandMy);
+    // printf("False result encode = %d\n", result);
 
-    result = sc_memorySet(50, commandMy);
-    printf("True Memory result = %d\n",result);
+    // result = sc_memorySet(50, commandMy);
+    // printf("True Memory result = %d\n",result);
 
-    result = sc_memorySet(101, commandMy);
-    printf("False memory result = %d", result);
+    // result = sc_memorySet(101, commandMy);
+    // printf("False memory result = %d", result);
 
-    sc_memorySave("file.bin");
-    sc_memoryInit();
-    sc_memoryLoad("file.bin");
+    // sc_memorySave("file.bin");
+    // sc_memoryInit();
+    // sc_memoryLoad("file.bin");
 
-    result = sc_memoryGet(0, &commandMy);
-    printf("%d", result);
+    // result = sc_memoryGet(0, &commandMy);
+    // printf("%d", result);
 
+    mt_clrscr();
+    mt_setbgcolor(CyanBack);
+    mt_setfgcolor(RedFore);
+    printf("HelloRED");
+    mt_default();
+    mt_gotoXY(5,3);
+    printf("lul");
 
 
     return 0;
--- SimpleCompLibrary/terminal.c
+++ SimpleCompLibrary/terminal.c
@@ -4,6 +4,34 @@
 int mt_clrscr(){
 
     printf("\E[H\E[J");
+
+    return 0;
+}
+
+int mt_default(){
     
+    printf("\E[0m");
+
+    return 0; 
+}
+
+int mt_gotoXY(int x, int y){
+
+    printf("\E[%d;%dH", x, y);
+
+    return 0;
+}
+
+int mt_setfgcolor(ForegroundColors color){
+
+    printf("\E[3%dm", color);
+
+    return 0;
+}
+
+int mt_setbgcolor(BackgroundColors color) {
+
+    printf("\E[4%dm", color);
+
     return 0;
 }
