9d06ba854787f886626f6762f69eda749137c296
frutlespoent <frutlespoent@gmail.com>
2021-02-14 16:39:07 +0700

    all .c and .h files here

---
+++ SimpleCompLibrary/main.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include "memory.h"
+#include "register.h"
+
+int main() {
+
+    int test;
+
+    sc_memoryInit();
+    sc_regInit();
+
+    int commandMy;
+    int result = sc_commandEncode(10,50, &commandMy);
+    printf("True result encode = %d\n", result); // 0= true, 1 - false
+
+    result = sc_commandEncode(12,128, &commandMy);
+    printf("False result encode = %d\n", result);
+
+    result = sc_memorySet(50, commandMy);
+    printf("True Memory result = %d\n",result);
+
+    result = sc_memorySet(101, commandMy);
+    printf("False memory result = %d", result);
+
+    sc_memorySave("file.bin");
+    sc_memoryInit();
+    sc_memoryLoad("file.bin");
+
+    result = sc_memoryGet(0, &commandMy);
+    printf("%d", result);
+
+
+
+    return 0;
+
+}
